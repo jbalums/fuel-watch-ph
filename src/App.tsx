4ae46bd6100527d51d7bdd9d86ec7e4b6217c605
Joel Baluma <baluma.joel91@gmail.com>
2026-03-26 22:40:06 +0800

    google oauth 2.0 implemented

--- src/App.tsx
+++ src/App.tsx
@@ -9,28 +9,33 @@ import Index from "./pages/Index";
 import Auth from "./pages/Auth";
 import Profile from "./pages/Profile";
 import NotFound from "./pages/NotFound";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+const clientSecret = import.meta.env.VITE_GOOGLE_CLIENT_SECRET;
 const queryClient = new QueryClient();
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <ThemeProvider>
-      <AuthProvider>
-        <TooltipProvider>
-          <Toaster />
-          <Sonner />
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="/auth" element={<Auth />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </BrowserRouter>
-        </TooltipProvider>
-      </AuthProvider>
-    </ThemeProvider>
-  </QueryClientProvider>
+	<QueryClientProvider client={queryClient}>
+		<GoogleOAuthProvider clientId={clientId}>
+			<ThemeProvider>
+				<AuthProvider>
+					<TooltipProvider>
+						<Toaster />
+						<Sonner />
+						<BrowserRouter>
+							<Routes>
+								<Route path="/" element={<Index />} />
+								<Route path="/auth" element={<Auth />} />
+								<Route path="/profile" element={<Profile />} />
+								<Route path="*" element={<NotFound />} />
+							</Routes>
+						</BrowserRouter>
+					</TooltipProvider>
+				</AuthProvider>
+			</ThemeProvider>
+		</GoogleOAuthProvider>
+	</QueryClientProvider>
 );
 
 export default App;
